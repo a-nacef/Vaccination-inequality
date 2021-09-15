a6e8854eaedbdfd0ec67375e52a5e3d21dc2c85c
nacef <aziznacef2@gmail.com>
2021-09-15 17:30:17 +0200

    Extract functional, working on transform stage

--- scripts/extract.py
+++ scripts/extract.py
@@ -1,2 +1,18 @@
+import requests
+import os
+
+
+
 def _extract():
-    print('h')
+    try:
+        v_data = requests.get("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.csv")
+    except requests.exceptions.ConnectionError:
+        print("err")
+    except requests.exceptions.RequestException:
+        print("err") 
+    print(v_data.content)
+    with open('v_data.csv', 'wb') as f:
+        f.write(v_data.content)
+
+
+_extract()
