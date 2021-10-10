e907e318a8849b36a012925ee14abd6029f028bf
nacef <aziznacef2@gmail.com>
2021-10-11 00:37:02 +0200

    Load functional with staging table and upsert

--- scripts/load.py
+++ scripts/load.py
@@ -9,17 +9,27 @@ def _load():
     curr = conn.cursor()
     print(os.environ['VPATH']+'staging/v-data_transformed.csv')
     try:
-   #     curr.execute(f"""COPY dimvax(location, day, daily_total, total, daily_per_million)
-                        #FROM '{os.environ['VPATH']+'staging/v-data_transformed.csv'}'
-                        #DELIMITER ','
-                        #CSV HEADER; 
-                        #""") 
-     curr.execute(f"""COPY dimgdp(name, year, gdppc)
-                        FROM '{os.environ['VPATH']+'staging/gdp-data.csv'}'
+        curr.execute("""DELETE FROM dimvax_staging WHERE location is not null """)
+        curr.execute(f"""COPY dimvax_staging(location, day, daily_total, total, daily_per_million)
+                        FROM '{os.environ['VPATH']+'staging/v-data_transformed.csv'}'
                         DELIMITER ','
                         CSV HEADER; 
-                        """) 
-
+                        """)
+        conn.commit() 
+        curr.execute("""
+            INSERT INTO dimvax(location, day, daily_total, total, daily_per_million) 
+                (SELECT * FROM dimvax_staging)
+                 ON CONFLICT ON CONSTRAINT dimvax_uq DO UPDATE SET
+                 daily_total = EXCLUDED.daily_total,
+                 total = EXCLUDED.total,
+                 daily_per_million = EXCLUDED.daily_per_million
+        """)
+#     curr.execute(f"""COPY dimgdp(name, year, gdppc)
+#                        FROM '{os.environ['VPATH']+'staging/gdp-data.csv'}'
+#                        DELIMITER ','
+#                        CSV HEADER; 
+         #               """) 
+#
     except psycopg2.Error as e:
         print(e.args)
     conn.commit()
